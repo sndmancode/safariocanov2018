bf207dfa6f1fe2ea4710628e094bbf8493072f85
Simon Roberts <simon@dancingcloudservices.com>
2018-11-28 10:34:12 -0700

    Day three mid-break

--- src/main/java/uselists/Ex1.java
+++ src/main/java/uselists/Ex1.java
@@ -4,17 +4,21 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Predicate;
 //import java.util.*;
 
+@FunctionalInterface
 interface StringTester {
   boolean test(String item);
 }
 
 class LongStringTester implements StringTester {
   private int threshold;
+
   public LongStringTester(int threshold) {
     this.threshold = threshold;
   }
+
   @Override
   public boolean test(String item) {
     return item.length() > threshold;
@@ -69,7 +73,7 @@ public class Ex1 {
 
   // Design pattern "Command"
   public static List<String> getNamesByCriterion
-      (List<String> ls, StringTester criterion) {
+  (List<String> ls, /*StringTester */ Predicate<String> criterion) {
     List<String> results = new ArrayList<>();
     for (String item : ls) {
       if (criterion.test(item)) {
@@ -108,13 +112,78 @@ public class Ex1 {
 //    System.out.println("Short: -----------------");
 //    System.out.println(getShortNames(names, 5));
 
-    System.out.println("Long:--------------------");
-    System.out.println(getNamesByCriterion(names, new LongStringTester(4)));
-    System.out.println("Longer:--------------------");
-    System.out.println(getNamesByCriterion(names, new LongStringTester(5)));
-    System.out.println("Shorter:--------------------");
-    System.out.println(getNamesByCriterion(names, new ShortStringTester()));
+//    System.out.println("Long:--------------------");
+//    System.out.println(getNamesByCriterion(names, new LongStringTester(4)));
+//    System.out.println("Longer:--------------------");
+//    System.out.println(getNamesByCriterion(names, new LongStringTester(5)));
+//    System.out.println("Shorter:--------------------");
+//    System.out.println(getNamesByCriterion(names, new ShortStringTester()));
+//    System.out.println("First Half:--------------------");
+//    System.out.println(getNamesByCriterion(names, new FirstHalfStringTester()));
+
+    System.out.println("Last Half:--------------------");
+    System.out.println(getNamesByCriterion(names,
+
+        // MUST be:
+        // an object! (so saying "new" is redundant)
+        // implement StringTester (otherwise, illegal argument!)
+
+        // IF (but only if!)
+        // the interface StringTester declares a SINGLE abstract method
+        // AND that is the ONLY method we wish to deine...
+        // then all the class clutter, and the signature parts of the method
+        // do not add to our understanding (nor the compiler's!!!)
+
+//        // Anonymous inner class
+//        new
+//        /*class LastHalfTester implements*/ StringTester() {
+//          @Override
+//          public boolean test(String item) {
+//            return item.charAt(0) > 'M';
+//          }
+//        }
+//        // Lambda v1
+//        /*new*/
+//        /*class LastHalfTester implements*/ /*StringTester() {*/
+//          /*@Override*/
+//          /*public boolean test*/(String item) -> {
+//            return item.charAt(0) > 'M';
+//          }
+//        /*}*/
+        // Lambda v2
+        (String item) -> {
+          return item.charAt(0) > 'M';
+        }
+    ));
+
+    System.out.println("Middle Letters:--------------------");
+    System.out.println(getNamesByCriterion(names,
+        (String item) -> {
+          char c = item.charAt(0);
+          return c > 'G' && c < 'T';
+        }
+    ));
+
+    // Must have context!!!!
+//    Object o = (String item) -> { return item.charAt(0) > 'M'; };
+//    StringTester o = (String item) -> { return item.charAt(0) > 'M'; };
+
     System.out.println("First Half:--------------------");
-    System.out.println(getNamesByCriterion(names, new FirstHalfStringTester()));
+    System.out.println(getNamesByCriterion(names,
+        item -> item.charAt(0) < 'M'
+    ));
+
+//    StringTester xx = item -> item.charAt(0) < 'M';
+    Predicate<String> xx = item -> item.charAt(0) < 'M';
+    System.out.println("xx is " + xx);
+    System.out.println("xx hashCode is " + xx.hashCode());
+    System.out.println("xx is of class " + xx.getClass().getName());
+//    System.out.println("xx is of class " + xx.getClass().getInterfaces());
+
+//    Object o = (String s) -> s.length() > 3;
+//    Predicate<String> ps = (StringBuilder sb) -> sb.length() > 3;
+//    Predicate<LocalDate> pld = x -> true;
+//    Predicate<String> ps = String s -> s.length() < 6;
+//    Predicate<String> ps = (s) -> return s.length() < 6;
   }
 }
